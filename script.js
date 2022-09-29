f5f58937270ab06bf62ab0218a8d89b2227148ad
Gabriel Ribeiro da Silva Neto <gabriel.ribeiro@tecnospeed.com.br>
2022-09-29 17:16:16 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function gerarTab() {
-    window.alert('Olá')
+    var val1 = document.getElementById('valor')
+    var res = document.getElementById('tab')
+
+    if (val1.value.lenght == 0){
+        window.alert('Por gentileza digite um número!')
+    } else {
+        let n = Number(val1.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <=10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            res.appendChild(item)
+            c++
+        }
+
+    }
+
+
 }
